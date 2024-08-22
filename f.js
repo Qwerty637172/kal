61ac41d37d9973a50a436d7fadd5c71adecdcd22
kalyan <kalyanlyrics@gmail.com>
2024-08-22 07:14:38 +0530

    kalki

---
+++ f.js
@@ -0,0 +1,15 @@
+let btn=document.querySelector("#btn");
+let bt=document.querySelector("#c");
+function k()
+{
+ btn.style.backgroundColor="blue";
+//  alert("fuck you");
+}
+function a()
+{
+ bt.style.backgroundColor='black';
+ bt.style.color="blue";
+}
+btn.addEventListener("click",k);
+// bt.addEventListener('mouseover',a);
+// console.log(btn);
